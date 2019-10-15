b2ddc99cfaa283b0b2c2f273ff7b7adce9e141c3
Ahmed Aziz <ahmed83@me.com>
2019-10-15 19:26:58 +0200

    change the stoprun docker config to show the time

--- stoprundocker.sh
+++ stoprundocker.sh
@@ -5,3 +5,6 @@ docker stop webshop-3d-psql
 docker rm webshop-3d-psql
 
 docker run --name webshop-3d-psql -p 5432:5432 -e POSTGRES_DB=webshop3d -e POSTGRES_PASSWORD=docker3d -d postgres
+
+now=$(date +"%r")
+echo "Current time : $now"
